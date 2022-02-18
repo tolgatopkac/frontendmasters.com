8b71f1174fafa2090da40064f21ce0c56564d5cb
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-02-18 11:26:24 +0300

    start Deep JS Foundations-v3 Kyle Simpson

---
+++ 03-Deep JavaScript Foundations v3 - Kyle Simpson/01 - Introduction/introduction.js
@@ -0,0 +1,36 @@
+// https://262.ecma-international.org/9.0/#Title
+
+/* whenever there's a divergence between what your brain thinks is happening, and what the computer does, that's where bugs enter the code
+ --getify's law#17
+ */
+
+//  Course Overview
+// Three Pillars
+/* 
+1) Types
+ - Primitive Types
+ - Abstract Operations
+ - Coercion
+ - Equality
+ - TypeScript, Flow, etc
+*/
+
+// it is irresponsible of you to write code and not understand this critical fact
+
+/*
+2) Scope
+ - Nested Scope
+ - Hoisting 
+ - Closure
+ - Modules
+*/
+
+/* 
+3) Objects (Oriented)
+ - this
+ - class { }
+ - Prototypes
+ -
+*/
+
+//Class :  I have not hidden my thoughts and options online about the class keyword and I will tell you again I firmly believe that it should not be used in JavaScript.
