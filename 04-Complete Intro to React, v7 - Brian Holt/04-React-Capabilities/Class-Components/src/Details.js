93d75611d3bb28994b7dce904637290069bf4079
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-08-24 06:12:46 +0300

    complete componentDidMount

--- 04-Complete Intro to React, v7 - Brian Holt/04-React-Capabilities/Class-Components/src/Details.js
+++ 04-Complete Intro to React, v7 - Brian Holt/04-React-Capabilities/Class-Components/src/Details.js
@@ -10,6 +10,16 @@ class Details extends Component {
     this.state = { loading: true };
   }
 
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    // this.setState(Object.assign({ loading: false }, json.pets[0]));
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
   render() {
     if (this.state.loading) {
       return <h2>loading ...</h2>;
@@ -32,7 +42,14 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
+
+// export default Details;
 
 // FUNCTION Component
 /* const Details = () => {
