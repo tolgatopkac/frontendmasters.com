0e2ac08831dd78c3f86d4bc4e7b1c48cb18fad90
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-10-04 08:25:38 +0300

    protect router

--- 07-API Design With Nodejs, v4 - Scott Moss/Day-1/Prisma/src/modules/auth.ts
+++ 07-API Design With Nodejs, v4 - Scott Moss/Day-1/Prisma/src/modules/auth.ts
@@ -17,14 +17,28 @@ export const createJWT = (user) => {
 export const protect = (req, res, next) => {
   const bearer = req.headers.authorization
 
+  // token
   if (!bearer) {
     res.status(401);
-    res.json("Not authorized")
+    res.json({ message: "Not authorized" })
     return
   }
 
+  const [, token] = bearer.split(" ")
+  if (!token) {
+    res.status(401);
+    res.json({ message: "Not valid token" })
+    return
+  }
 
-
-
-
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = user;
+    next();
+  } catch (e) {
+    console.error(e)
+    res.status(401);
+    res.json({ message: "Not valid token" })
+    return
+  }
 }
