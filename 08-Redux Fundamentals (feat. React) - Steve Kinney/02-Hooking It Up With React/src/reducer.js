d35f08b3e83ff3dab4d1f5e6f916eb631f1b77b5
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-10-09 08:24:18 +0300

    add create setCounter.js

--- 08-Redux Fundamentals (feat. React) - Steve Kinney/02-Hooking It Up With React/src/reducer.js
+++ 08-Redux Fundamentals (feat. React) - Steve Kinney/02-Hooking It Up With React/src/reducer.js
@@ -2,7 +2,7 @@
 
 import { DECREMENT, INCREMENT, SET } from './actions';
 
-export const initialState = { count: 0 };
+export const initialState = { count: 400 };
 
 export const reducer = (state = initialState, action) => {
   if (action.type === INCREMENT) {
--- 08-Redux Fundamentals (feat. React) - Steve Kinney/02-Hooking It Up With React/src/store.js
+++ 08-Redux Fundamentals (feat. React) - Steve Kinney/02-Hooking It Up With React/src/store.js
@@ -4,4 +4,8 @@
 import { createStore } from 'redux';
 import { reducer } from './reducer';
 
-export const store = createStore(reducer);
+// Redux Tools Extension
+const enhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+export const store = createStore(reducer, enhancer);
