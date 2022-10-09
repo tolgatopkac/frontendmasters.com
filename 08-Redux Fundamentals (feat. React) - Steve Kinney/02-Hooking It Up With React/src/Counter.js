53ae7611ddda49b06c9d5b67541dc21cc8aacd26
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-10-09 08:28:40 +0300

    bindActionCreators example

--- 08-Redux Fundamentals (feat. React) - Steve Kinney/02-Hooking It Up With React/src/Counter.js
+++ 08-Redux Fundamentals (feat. React) - Steve Kinney/02-Hooking It Up With React/src/Counter.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { decrement, increment, set } from './actions';
 import { SetCounter } from './SetCounter';
 
@@ -7,18 +8,16 @@ export const Counter = () => {
   const count = useSelector((state) => state.count);
   const dispatch = useDispatch();
 
+  const actions = bindActionCreators({ increment, decrement, set }, dispatch);
+
   return (
     <main className="Counter">
       <h1>Days Since Last {incident}</h1>
       <p className="count">{count}</p>
       <section className="controls">
-        <button onClick={() => dispatch(increment(count + 1))}>
-          Increment
-        </button>
-        <button onClick={() => dispatch(set(0))}>Reset</button>
-        <button onClick={() => dispatch(decrement(count - 1))}>
-          Decrement
-        </button>
+        <button onClick={() => actions.increment()}>Increment</button>
+        <button onClick={() => actions.set(0)}>Reset</button>
+        <button onClick={() => actions.decrement()}>Decrement</button>
       </section>
       <SetCounter />
     </main>
