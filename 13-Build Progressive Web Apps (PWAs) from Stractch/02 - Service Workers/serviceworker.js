1dab100c3317045bd5a22fc7bb80ff7ffa20d53d
Tolga <tolgatopkac09@gmail.com>
2023-01-01 04:49:14 +0300

    ServiceWorker/serving all assets

--- 13-Build Progressive Web Apps (PWAs) from Stractch/02 - Service Workers/serviceworker.js
+++ 13-Build Progressive Web Apps (PWAs) from Stractch/02 - Service Workers/serviceworker.js
@@ -16,5 +16,12 @@ self.addEventListener("fetch",event=> {
     if(event.request.url == "http://localhost:3000/fake"){
             const response = new Response(`hello I'm a response on URL ${event.request.url}`);
         event.respondWith(response);
+    } else {
+        //  we want to try and see if the request is cached
+        caches.open("assets").then(cache => {
+            cache.match(event.request).then(cachedResponse => {
+                event.respondWith(cachedResponse);
+            })
+        })
     }
 })
