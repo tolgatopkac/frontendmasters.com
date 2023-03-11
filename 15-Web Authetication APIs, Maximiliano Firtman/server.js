0343f52d97b3dfb09990054c9b5412f783204ab8
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-03-11 03:33:33 +0300

    create register endpoint

--- 15-Web Authetication APIs, Maximiliano Firtman/server.js
+++ 15-Web Authetication APIs, Maximiliano Firtman/server.js
@@ -1,42 +1,71 @@
-import express from 'express'
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
-import * as url from 'url';
-import bcrypt from 'bcryptjs';
-import * as jwtJsDecode from 'jwt-js-decode';
+import express from "express";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+import * as url from "url";
+import bcrypt from "bcryptjs";
+import * as jwtJsDecode from "jwt-js-decode";
 import base64url from "base64url";
-import SimpleWebAuthnServer from '@simplewebauthn/server';
+import SimpleWebAuthnServer from "@simplewebauthn/server";
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const adapter = new JSONFile(__dirname + '/auth.json');
+const adapter = new JSONFile(__dirname + "/auth.json");
 const db = new Low(adapter);
 await db.read();
-db.data ||= { users: [] }
+db.data ||= { users: [] };
 
 const rpID = "localhost";
 const protocol = "http";
 const port = 5050;
 const expectedOrigin = `${protocol}://${rpID}:${port}`;
 
-app.use(express.static('public'));
+// static serving public folder
+app.use(express.static("public"));
+// read body parsing json
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// check user
+function findUser(email) {
+  const results = db.data.users.filter((u) => u.email == email);
+  if (results.length == 0) return undefined;
+  return results[0];
+}
 
 // ADD HERE THE REST OF THE ENDPOINTS
+app.post("/auth/register", (req, res) => {
+  // req = get all the information coming the browser
+  // res = send information the browser
+  // TODO : DATA VALIDATION
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-
+  const userFound = findUser(user.email);
+  if (userFound) {
+    // user already existed
+    res.send({ ok: false, message: "User already exists" });
+  } else {
+    // user is new, we are good!
+    db.data.users.push(user);
+    db.write();
+    res.send({ ok: true });
+  }
+});
 
 app.get("*", (req, res) => {
-    res.sendFile(__dirname + "public/index.html"); 
+  res.sendFile(__dirname + "public/index.html");
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App listening on port ${port}`);
 });
-
