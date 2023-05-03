c1d1f523c6ede397cdd81b80419c834f9ecf8bab
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-05-03 23:51:02 +0300

    🎨 style(+page.svelte, Game.svelte, styles.css): add styles to the game page The styles.css file is added to the project, which contains global styles for the game page. The Game.svelte file now contains a div with class "game" that has a flex layout and two child divs with classes "info" and "grid-container". The +page.svelte file now imports the styles.css file. These changes improve the visual appearance of the game page.

---
+++ 18-Svelte & SvelteKit, with Rich Harris/ematchi-game/src/routes/Game.svelte
@@ -0,0 +1,27 @@
+<div class="game">
+	<div class="info" />
+	<div class="grid-container" />
+	<div class="info" />
+</div>
+
+<style>
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		/* height: 100%; */
+	}
+
+	.info {
+		width: 80vmin;
+		height: 10vmin;
+		background: purple;
+	}
+
+	.grid-container {
+		width: 80vmin;
+		height: 80vmin;
+		background: teal;
+	}
+</style>
