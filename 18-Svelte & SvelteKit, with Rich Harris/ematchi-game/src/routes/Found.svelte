cfd4d58418e7059f18c1b3c32ce132c1c253cac8
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-05-04 21:57:51 +0300

    🎨 style(Found.svelte): add drop shadow to the container and style the pairs The container now has a drop shadow to give it a more 3D look. The pairs are now centered and have a white background with a padding of 1em and a border radius of 50% to make them circular. The size of the images has been reduced to 2em x 2em.

--- 18-Svelte & SvelteKit, with Rich Harris/ematchi-game/src/routes/Found.svelte
+++ 18-Svelte & SvelteKit, with Rich Harris/ematchi-game/src/routes/Found.svelte
@@ -21,16 +21,23 @@
 		align-items: center;
 		gap: 0.5em;
 		height: 100%;
+		filter: drop-shadow(0.2em 0.2em 0.5em rgba(0, 0, 0, 0.2));
 	}
 	.pair {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		width: 4em;
 		height: 4em;
+		background: white;
+		padding: 1em;
+		border-radius: 50%;
 	}
 	img {
 		position: absolute;
-		left: 0;
-		top: 0;
-		width: 4em;
-		height: 4em;
+		/* left: 0;
+		top: 0; */
+		width: 2em;
+		height: 2em;
 	}
 </style>
